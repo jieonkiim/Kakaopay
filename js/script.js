9556efe33499eb187dbc37837853804c4a6afe5e
jieonkiim <jaykimma920@gmail.com>
2022-02-07 17:39:08 +0900

    header구현

--- js/script.js
+++ js/script.js
@@ -1,24 +1,32 @@
 
-
-// // 햄버거 클릭 event 
-// const button=()=> {
-//     const burger = document.querySelector('.burger');   // burger 클래스 가져오기
-//     burger.addEventListener.on('click', ()=> {             // 클릭 하면 !!
-//         burger.classList.toggle('toggle');              // toggle 클래스 있으면 제거, 없으면 추가
-//     })
-// }
-
-// button();
-
-
-// const burger = document.querySelector('.burger');
-
-// burger.addEventListener('click', (event) => {
-//     event.currentTarget.classList.toggle('toggle');
-// });
-
 $(document).ready(function() {
-    $(".burger").on('click', function() {
-        $(this).toggleClass("toggle");
-    });
+
+    // 메뉴에 마우스 올리면, 배경 흰색
+    $('.menu ul.main li').on('mouseenter', function() {
+        $('header').css({'background-color': 'var(--white)' })
+        $('.menu .main>li').css({'color' : 'var(--black'} )
+        $('.logo a').css({'color' : 'var(--black'} )
+        $('header').stop().animate({'height': '84px'}, 400)
+    })
+    
+    // 서브메뉴에 마우스올리면, 헤더 높이를 늘려준다.
+    $('.menu ul.main li.pmenu').on('mouseenter', function() {
+
+        // 서브메뉴 보여주기
+        let li = $(this)
+        $('.menu2').css({'display': 'none'})
+        li.children('.menu2').css({'display': 'block'})
+        $('header').stop().animate({'height': '300px'}, 400)
+    })
+
+    // 헤더를 벗어나면 배경 투명
+    $('header').on('mouseleave', function() {
+        $('.menu .main>li').css({'color' : 'var(--white'} )
+        $('.logo a').css({'color' : 'var(--white'} )
+        $('header').stop().animate({'height': '84px'}, 400)
+        $('header').css({'background-color': 'transparent' })
+        
+    })
+    
+    
 })
